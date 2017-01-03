31376f5d19f964ed3fc78b430e19f7c991cbb454
Brittany Storoz <brittanystoroz@gmail.com>
2017-01-03 17:02:36 -0500

    DB Starting point

--- app.js
+++ app.js
@@ -1,30 +1,4 @@
-function loadMarkdown(markdownFileId) {
-  let objectStore = db.transaction(DB_STORE_NAME, 'readwrite').objectStore(DB_STORE_NAME);
-  let request = objectStore.get(markdownFileId);
-
-  request.onerror = function(event) {
-    console.log('Request Error');
-  };
-
-  request.onsuccess = function(event) {
-    let mdResult = request.result.markdownContent;
-    let mdFileName = request.result.fileName;
-
-    $('#file-name').val(mdFileName)
-    $('#live-markdown').val(mdResult);
-    updatePreview(mdResult);
-  };
-}
-
 $('#live-markdown').on('keyup', event => updatePreview(event.currentTarget.value));
-
-navigator.serviceWorker.addEventListener('message', message => {
- if (message.data.updateRecordCount) {
-  let currentCount = parseInt($('.counter').text());
-  $('.counter').text(currentCount + 1);
-  $('#markdown-records').append(`<option value=${message.data.recordId}>${message.data.fileName}</option>`);
- }
-});
 
 function updatePreview(markdownContent) {
   const md = markdownit();
@@ -47,7 +21,6 @@ if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('./service-worker.js')
       .then(registration => {
-        Notification.requestPermission();
         enableSubmitButton();
       }).catch(err => {
         console.log(`ServiceWorker registration failed: ${err}`);
--- service-worker.js
+++ service-worker.js
@@ -21,49 +21,5 @@ self.addEventListener('fetch', event => {
 });
 
 self.addEventListener('message', event => {
-  let mdContent = event.data.mdContent;
-  let mdFileName = `${event.data.mdFileName}.md`;
-  let dbReq = indexedDB.open('mdFileHistory');
-
-  dbReq.onsuccess = (event) => {
-    console.log('DB opened from service worker');
-    
-    let db = event.target.result;
-    let transaction = db.transaction(['mdFiles'], 'readwrite');
-
-    transaction.oncomplete = (event) => {
-      console.log('Transaction Success');
-    }
-
-    transaction.onerror = (event) => {
-      console.log('Transaction Error');
-    }
-
-    let objectStore = transaction.objectStore("mdFiles");
-    let objectStoreReq = objectStore.add({ 
-      fileName: mdFileName,
-      authorName: 'Brittany Storoz',
-      markdownContent: mdContent
-    });
-
-    objectStoreReq.onsuccess = (event) => {
-      console.log('objectStore request succeeded');    
-      self.clients.matchAll().then(clients => 
-        clients[0].postMessage({
-          'updateRecordCount': true,
-          'recordId': event.target.result,
-          'fileName': mdFileName
-        })
-      );
-    }
-
-    objectStoreReq.onerror = (event) => {
-      console.log('objectStore request failed');
-    };
-  };
-
-  dbReq.onerror = (event) => {
-    console.log('DB not opened from sw');
-  };
-
+  // handle messages to store markdown in IndexedDB
 });
