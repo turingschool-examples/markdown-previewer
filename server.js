5ebbfdb23b8a88a655e09748f484a748a0c62fe5
Brittany Storoz <brittanystoroz@gmail.com>
2017-12-06 12:21:29 -0700

    Add middleware

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ const bodyParser = require('body-parser');
 const fs = require('fs');
 const path = require('path');
 
+const requireHTTPS = (request, response, next) => {
+  if (request.header('x-forwarded-proto') != 'https') {
+    return response.redirect(`https://${request.header('host')}${request.url}`);
+  }
+  next();
+};
+
+if (process.env.NODE_ENV === 'production') { app.use(requireHTTPS); }
+
 app.set('port', process.env.PORT || 3000);
 
 app.use(bodyParser.json());
