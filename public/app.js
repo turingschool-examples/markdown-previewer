2ca53adbb238224eb080a267420c72f7d4c5d7e8
Brittany Storoz <brittanystoroz@gmail.com>
2017-12-05 19:51:45 -0700

    Rollback to beginning

--- public/app.js
+++ public/app.js
@@ -5,16 +5,3 @@ function updatePreview(event) {
   let htmlResult = md.render(event.currentTarget.value);
   $('#html-preview').html(htmlResult);
 };
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('./service-worker.js')
-      .then(registration => {
-        // Registration was successful
-        console.log('ServiceWorker registration successful');
-      }).catch(err => {
-        // registration failed :(
-        console.log(`ServiceWorker registration failed: ${err}`);
-      });
-  });
-}
