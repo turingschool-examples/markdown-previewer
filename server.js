924dc78fc80c608a1f221aeaa277813e86382a82
Brittany Storoz <brittanystoroz@gmail.com>
2017-12-06 15:26:45 -0700

    Add server content

--- server.js
+++ server.js
@@ -19,8 +19,23 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 
+app.locals.markdowns = [
+  { id: 1512598678712, title: Lorem, content: '# hey \n## whats up' },
+  { id: 1512598678716, title: Ipsum, content: '# bye \n## go away' },
+  { id: 1512598678719, title: Dolor, content: '# yoo \n## set amet' }
+];
+
 app.get('/', (request, response) => {
   response.sendFile('index.html');
+});
+
+app.get('/api/v1/markdowns', (request, response) => {
+  response.status(200).json(app.locals.markdowns);
+});
+
+app.post('/api/v1/markdowns', (request, response) => {
+  app.locals.markdowns.push(request.body);
+  response.status(201).json(app.locals.markdowns);
 });
 
 app.listen(app.get('port'), () => { 
