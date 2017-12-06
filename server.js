c42df1be8172dcc6797cbe75f94a595dedb81239
Brittany Storoz <brittanystoroz@gmail.com>
2017-12-05 20:47:20 -0700

    Fix redirect

--- server.js
+++ server.js
@@ -5,24 +5,31 @@ const bodyParser = require('body-parser');
 const fs = require('fs');
 const path = require('path');
 
+const requireHTTPS = (request, response, next) => {
+  if (request.header('x-forwarded-proto') != 'https') {
+    return res.redirect(`https://${request.header('host')}${request.url}`);
+  }
+  next();
+};
+
 app.set('port', process.env.PORT || 3001);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 // app.use('/', httpsRedirect);
+app.use(requireHTTPS);
 
-app.get('*',function(req,res,next){
-  console.log("MIDDLEWARE FIRING: ", req.headers);
-  if (req.headers['x-forwarded-proto'] != 'https') {
-    return res.redirect('https://' + req.get('host') + req.url);
-  }
-  next();
-})
+// app.get('*',function(req,res,next){
+//   console.log("MIDDLEWARE FIRING: ", req.headers);
+//   if (req.headers['x-forwarded-proto'] != 'https') {
+//     return res.redirect('https://' + req.get('host') + req.url);
+//   }
+//   next();
+// })
 
 
 app.get('/', (request, response) => {
-  console.log("FROM INITIAL GET: ", req.headers);
   response.sendFile('index.html');
 });
 
