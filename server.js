d75d3a7932d7dd93d75deac72d00d3a7df1854bd
Brittany Storoz <brittanystoroz@gmail.com>
2017-12-06 08:37:09 -0700

    remove unused code

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-// const httpsRedirect = require('express-https-redirect');
 const app = express();
 const bodyParser = require('body-parser');
 const fs = require('fs');
@@ -17,17 +16,7 @@ app.set('port', process.env.PORT || 3000);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
-// app.use('/', httpsRedirect);
 app.use(requireHTTPS);
-
-// app.get('*',function(req,res,next){
-//   console.log("MIDDLEWARE FIRING: ", req.headers);
-//   if (req.headers['x-forwarded-proto'] != 'https') {
-//     return res.redirect('https://' + req.get('host') + req.url);
-//   }
-//   next();
-// })
-
 
 app.get('/', (request, response) => {
   response.sendFile('index.html');
